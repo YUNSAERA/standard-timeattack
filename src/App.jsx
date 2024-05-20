efd55185fa1d30626041c908457e91c6113e14f0
YUNSAERA <smctofk96@gmail.com>
2024-05-20 16:57:16 +0900

    todoform 제목까지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-export default function App() {
+import React, { useState } from "react";
+import "./APP.css";
+import TodoContainer from "components/TodoContainer";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: Date.now(),
+      title: "순공 시간 확인하기",
+      const: "순공 시간 체크해서 공부해봅시다.",
+      isDone: true,
+    },
+  ]);
+
+  const clickAddTodoHandler = () => {
+    if (title.trim() && content.trim()) {
+      setTodos([...todos, { id: Date.now(), title, content, isDone: false }]);
+      setTitle("");
+      setContent("");
+    }
+  };
+
+  const toggleCompletionHandler = (id) => {
+    setTodos(
+      todos.map((item) =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
+  const clickDeleteHandler = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
+  };
   return (
-    <>
-      <h1>투두리스트 타임어택</h1>
-    </>
+    <TodoContainer
+      setTitle="My Todo List"
+      siteSubTitle="React"
+      todos={todos}
+      title={title}
+      setTitle={setTitle}
+      content={content}
+      clickaddTodoHandler={clickAddTodoHandler}
+      toggleCompletionHandler={toggleCompletionHandler}
+      clickDeleteHandler={clickDeleteHandler}
+    />
   );
 }
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <>
+//       <h1>투두리스트 타임어택</h1>
+//     </>
+//   );
+// }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,3 +1,19 @@
-export default function TodoForm() {
-  return <div>TodoForm</div>;
+import React from "react";
+
+function TodoForm({
+  title,
+  setTitle,
+  content,
+  setContent,
+  clickAddTodoHandler,
+}) {
+  return(
+    <div className="inputArea">
+      <div className="titleText">제목 :</div>
+      <input
+      className="inputTitle"
+      value={title}
+      onChange = {(event)=> setTitle(event.target.value)}></input>
+    </div>
+  )
 }
