e44c67baaf4210d0a05b197fd1989e95e3c3ae5a
YUNSAERA <smctofk96@gmail.com>
2024-05-20 16:10:25 +0900

    버튼까지

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import {useState} from "react"
+import './App.css';
+
+
 export default function App() {
+    const [value, setValue] = useState(0);
+    const [result, setResult] = useState(0);
+
+
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input /> 만큼을
+        
+        <button onClick = {()=>handAdd()}>더할게요</button> 
+        <button onClick = {()=>handSubtract()}>뺄게요</button> 
+        <button onClick = {()=>handleReset()}>초기화</button> 
       </div>
       <hr />
-      <div>
+      <div className="result-container">
         <h3>결과</h3>
-        <p>0</p>
       </div>
     </div>
   );
